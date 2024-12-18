bf5fb60c9bbd8842da38f42b6870f3b371b76e49
yue002 <114208281+yue002@users.noreply.github.com>
2024-12-18 15:52:00 +0700

    Update Lec05.java

--- Lec05.java
+++ Lec05.java
@@ -2,10 +2,12 @@ public class Lec05 extends Users {
     public static void main(String[] args) {
         Users John = new Users("John", 1954, 2, 18);
         John.displayInfo();
-
+        System.out.println();
         Admin nicolas = new Admin("Nicolas", 1964, 1, 7);
         nicolas.displayInfo();
+        System.out.println();
         nicolas.displayInfo(true);
+        System.out.println();
         nicolas.displayInfo(false);
 
     }
